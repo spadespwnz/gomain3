d3d86e251e0b01e64441a9398d42071e4bcc4269
Nathan Brown <spades@live.no>
2018-07-15 15:26:34 -0400

    first

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"Gomain/controllers"
+	"log"
+	"net/http"
+)
+
+func main() {
+	apiController := controllers.NewApiController()
+	defer apiController.Db.Close()
+	http.HandleFunc("/get", apiController.GetData)
+	http.HandleFunc("/post", apiController.PostData)
+	http.HandleFunc("/list", apiController.GetList)
+	http.HandleFunc("/add/", apiController.AddList)
+	http.HandleFunc("/drop/", apiController.DropDb)
+	http.HandleFunc("/AddJpWord", apiController.AddJpWord)
+	http.HandleFunc("/FindAll", apiController.FindAll)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
